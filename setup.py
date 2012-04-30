b144af1774213f7b784022cfe18a6a5eb3a066eb
srossross <srossross@enthought.com>
2012-04-30 10:35:47 -0300

    ENH: added setup and base project.

---
+++ setup.py
@@ -0,0 +1,48 @@
+
+from setuptools import setup, find_packages, Extension
+
+def main():
+    try:
+        long_description = open('README.rst').read()
+    except IOError as err:
+        long_description = str(err)
+    try:
+        version_str = open('version.txt').read()
+    except IOError as err:
+        version_str = '???'
+        
+
+    setup(
+        name='modlr',
+        version=version_str,
+        
+        author='Agile Geoscience.',
+        author_email='hello@agilegeoscience.com',
+        url='https://github.com/agile-geoscience/modelr',
+        
+        packages=find_packages(),
+            
+        classifiers=[c.strip() for c in """\
+            Development Status :: 5 - Production/Stable
+            Intended Audience :: Developers
+            Intended Audience :: Science/Research
+            License :: OSI Approved :: BSD License
+            Operating System :: MacOS
+            Operating System :: Microsoft :: Windows
+            Operating System :: OS Independent
+            Operating System :: POSIX
+            Operating System :: Unix
+            Programming Language :: Python :: 2
+            Topic :: Scientific/Engineering
+            Topic :: Software Development
+            Topic :: Software Development :: Libraries
+            """.splitlines() if len(c.strip()) > 0],
+        description='Web Service like google charts API',
+        long_description=long_description,
+        license='BSD',
+        
+          )
+
+
+if __name__ == '__main__':
+    main()
