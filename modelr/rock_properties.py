7536120fb35d5989610355f89a7612cf9446057e
srossross <srossross@enthought.com>
2012-05-04 11:00:38 -0300

    ENH: updated reflectivity functions to take RockProperties as arguments.

--- modelr/rock_properties.py
+++ modelr/rock_properties.py
@@ -6,7 +6,9 @@ modelr.rock_properties -- TODO short docstring here
 TODO: long doc here 
 '''
 
-from . import zoep_calc
+from modelr.reflecty import zoeppritz, akirichards_alt, akirichards, fatti,\
+    shuey3, shuey2, bortfeld2, bortfeld3
+    
 class RockProperties(object):
     '''
     Class to store rock properties.
@@ -37,6 +39,40 @@ def zoeppritz(Rp0, Rp1, theta1):
     :param theta1:
      
     '''
-    return zoep_calc.zoeppritz(Rp0.vp, Rp0.vs, Rp0.rho,
-                               Rp1.vp, Rp1.vs, Rp1.rho,
+    return zoeppritz(Rp0.vp, Rp0.vs, Rp0.rho,
+                     Rp1.vp, Rp1.vs, Rp1.rho,
                                theta1)    
+
+
+
+def akirichards(Rp0, Rp1, theta1):
+    akirichards(Rp0.vp, Rp0.vs, Rp0.rho, Rp1.vp, Rp1.vs, Rp1.rho, theta1)
+
+def akirichards_alt(Rp0, Rp1, theta1):
+    akirichards_alt(Rp0.vp, Rp0.vs, Rp0.rho, Rp1.vp, Rp1.vs, Rp1.rho, theta1)
+
+def fatti(Rp0, Rp1, theta1):
+    fatti(Rp0.vp, Rp0.vs, Rp0.rho, Rp1.vp, Rp1.vs, Rp1.rho, theta1)
+
+def shuey2(Rp0, Rp1, theta1):
+    shuey2(Rp0.vp, Rp0.vs, Rp0.rho, Rp1.vp, Rp1.vs, Rp1.rho, theta1)
+
+def shuey3(Rp0, Rp1, theta1):
+    shuey3(Rp0.vp, Rp0.vs, Rp0.rho, Rp1.vp, Rp1.vs, Rp1.rho, theta1)
+
+def bortfeld2(Rp0, Rp1, theta1):
+    bortfeld2(Rp0.vp, Rp0.vs, Rp0.rho, Rp1.vp, Rp1.vs, Rp1.rho, theta1)
+    
+def bortfeld3(Rp0, Rp1, theta1):
+    bortfeld3(Rp0.vp, Rp0.vs, Rp0.rho, Rp1.vp, Rp1.vs, Rp1.rho, theta1)
+    
+MODELS = {
+          'zoeppritz': zoeppritz,
+          'akirichards': akirichards,
+          'akirichards_alt': akirichards_alt,
+          'fatti': fatti,
+          'shuey2': shuey2,
+          'shuey3': shuey3,
+          'bortfeld2': bortfeld2,
+          'bortfeld3': bortfeld3,
+          }
