d1b1e0bb52d9889137aaab1b98f015b8559253e8
EvanBianco <evan@agilegeoscience.com>
2013-10-25 13:13:22 -0300

    made a wedge with color as a new input

--- modelr/web/scripts/wedge_emb.py
+++ modelr/web/scripts/wedge_emb.py
@@ -20,22 +20,23 @@ def add_arguments(parser):
     parser.add_argument('max_thickness', default=50, type=int, help='The maximum thickness of the wedge')
     parser.add_argument('ntraces', default=300, type=int, help='Number of traces')
     
-    parser.add_argument('Layer0', type=rock_properties_type, 
+    parser.add_argument('Rpp0', type=rock_properties_type, 
                         help='rock properties of upper rock', required=True)
-    parser.add_argument('Layer1', type=rock_properties_type, 
+    parser.add_argument('Rpp1', type=rock_properties_type, 
                         help='rock properties of lower rock', required=True)
     
     parser.add_argument('theta', type=float, help='Angle of incidence')
     
     parser.add_argument('f', type=float, help='frequency', default=25)
+    parser.add_argument('color', type=str, help='Matplotlib colormap', default='Grey')
     return parser
 
 def run_script(args):
     
     matplotlib.interactive(False)
     
-    Rprop0 = args.Layer0 
-    Rprop1 = args.Layer1
+    Rprop0 = args.Rpp0 
+    Rprop1 = args.Rpp1
     
     warray_amp = create_wedge(args.ntraces, args.pad, args.max_thickness,
                               Rprop0, Rprop1, args.theta, args.f)
@@ -44,7 +45,7 @@ def run_script(args):
     ax1 = fig.add_subplot(111)
 
     aspect = float(warray_amp.shape[1]) / warray_amp.shape[0]
-    ax1.imshow( warray_amp, aspect=aspect, cmap='hot')
+    ax1.imshow( warray_amp, aspect=aspect, cmap=args.color)
     
     plt.title(args.title % locals())
     plt.ylabel('time (ms)')
