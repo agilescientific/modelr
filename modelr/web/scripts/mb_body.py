06956941b5e1def693e77eed33b83c219b2b32ca
kwinkunks <matt@agilegeoscience.com>
2013-11-15 15:42:09 -0400

    mb_body is awsm

--- modelr/web/scripts/mb_body.py
+++ modelr/web/scripts/mb_body.py
@@ -3,21 +3,18 @@ Created on Apr 30, 2012
 
 @author: Sean Ross-Ross, Matt Hall, Evan Bianco
 '''
-import numpy as np
+
 import matplotlib
 import matplotlib.pyplot as plt
 
 from argparse import ArgumentParser
+from modelr.web.defaults import default_parsers
 
 from modelr.reflectivity import get_reflectivity, do_convolve
 
 import modelr.modelbuilder as mb
 
-from modelr.web.urlargparse import rock_properties_type, reflectivity_type, wavelet_type
-from modelr.web.urlargparse import WAVELETS
-
-from modelr.rock_properties import MODELS
-
+from modelr.web.urlargparse import rock_properties_type
 from modelr.web.util import return_current_figure
 from modelr.web.util import wiggle
 
@@ -26,29 +23,19 @@ short_description = 'Create a simple wedge model.'
 
 def add_arguments(parser):
     
-    parser.add_argument('title', 
-                        default='Plot',
-                        type=str,
-                        help='The title of the plot'
-                        )
-                        
-    parser.add_argument('pad',
-                        default=50,
-                        type=int,
-                        help='The time in milliseconds above and below the wedge'
-                        )
-                        
-    parser.add_argument('thickness',
-                        type=int,
-                        default=50,                        
-                        help='The maximum thickness of the wedge'
-                        )
-                        
-    parser.add_argument('ntraces',
-                        default=300,
-                        type=int,
-                        help='Number of traces'
-                        )
+    default_parser_list = ['ntraces',
+                           'pad',
+                           'colour',
+                           'reflectivity_method',
+                           'title',
+                           'theta',
+                           'f',
+                           'display',
+                           'colour',
+                           'wavelet'
+                           ]
+    
+    default_parsers(parser,default_parser_list)
     
     parser.add_argument('Rock0',
                         type=rock_properties_type, 
@@ -71,50 +58,31 @@ def add_arguments(parser):
                         default='2500,1200,2600'
                         )
     
-    parser.add_argument('reflectivity_method',
-                        type=reflectivity_type,
-                        help='Algorithm for calculating reflectivity',
-                        default='zoeppritz',
-                        choices=MODELS.keys()
-                        ) 
-                         
-    parser.add_argument('theta',
-                        type=int,
-                        help='Angle of incidence',
-                        default=0
-                        )
-                        
-    parser.add_argument('f',
+    parser.add_argument('left',
                         type=int,
                         action='list',
-                        help='Frequency of wavelet',
-                        default=25
+                        default='0,0',                        
+                        help='The thickness on the left-hand side'
                         )
                         
-    parser.add_argument('colour',
-                        type=str,
-                        help='Matplotlib colourmap, ageo.co/modelrcolour',
-                        default='Greys'
+    parser.add_argument('right',
+                        type=int,
+                        action='list',
+                        default='0,50',                        
+                        help='The thickness on the right-hand side'
                         )
-    
-    parser.add_argument('display',
-                        type=str,
-                        help='Type of seismic display',
-                        choices=['wiggle', 'variable-density', 'both'],
-                        default='variable-density'
-                        )
-
-    parser.add_argument('wavelet',
-                        type=wavelet_type,
-                        help='Wavelet type',
-                        default='ricker',
-                        choices=WAVELETS.keys()
-                        )
-
+                        
     parser.add_argument('margin',
                         type=int,
                         help='Traces with zero thickness',
                         default=1
+                        )
+
+    parser.add_argument('selection',
+                        type=str,
+                        help='Slice to return',
+                        default='thickness',
+                        choices=['thickness', 'offset', 'frequency']
                         )
 
     return parser
@@ -123,14 +91,18 @@ def add_arguments(parser):
 def run_script(args):
     
     matplotlib.interactive(False)
-
-    # Get the physical model (an array of rocks)    
-    model = mb.wedge(traces = args.ntraces,
+    
+    left = (args.left[0], args.left[1])
+    right = (args.right[0], args.right[1])
+    
+    model = mb.body(traces = args.ntraces,
                    pad = args.pad,
                    margin=args.margin,
-                   thickness = args.thickness,
+                   left = left,
+                   right = right,
                    layers = (args.Rock0,args.Rock1,args.Rock2)
                    )
+        
 
     colourmap = { 0: args.Rock0, 1: args.Rock1 }
     if not isinstance(args.Rock2, str):
@@ -143,7 +115,12 @@ def run_script(args):
                                     reflectivity_method = args.reflectivity_method
                                     )
     
-    warray_amp = do_convolve(args.wavelet, args.f, reflectivity)
+    if args.selection == 'frequency':
+        warray_amp = do_convolve(args.wavelet, args.f, reflectivity)                        
+    elif args.selection == 'offset':
+        warray_amp = do_convolve(args.wavelet, args.f, reflectivity)
+    else:
+        warray_amp = do_convolve(args.wavelet, args.f, reflectivity)
         
                         
     fig = plt.figure()
