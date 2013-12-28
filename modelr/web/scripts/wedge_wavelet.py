553011729bf271d254d9eb48510a40d932d5e457
ben-bougher <ben.bougher@gmail.com>
2013-12-27 22:06:37 -0400

    renamed scripts, added more wedges

--- modelr/web/scripts/wedge_wavelet.py
+++ modelr/web/scripts/wedge_wavelet.py
@@ -5,25 +5,23 @@ Created on Apr 30, 2012
 '''
 import numpy as np
 import matplotlib
-
 import matplotlib.pyplot as plt
 
 from argparse import ArgumentParser
 from modelr.web.defaults import default_parsers
 from modelr.web.urlargparse import rock_properties_type
-from agilegeo.avo import zoeppritz
 
 from modelr.web.util import modelr_plot
-from agilegeo.wavelet import ricker
 
 import modelr.modelbuilder as mb
-
+from agilegeo.avo import zoeppritz
+from agilegeo.wavelet import ricker
 from svgwrite import rgb
 
-short_description = 'Spatial view of a simple wedge model'
+# This is required for Script help
+short_description = 'Wavelet bank in a wedge model.'
 
 def add_arguments(parser):
-    
     default_parser_list = [
                            'base1','base2','overlay1','overlay2',
                            'opacity'
@@ -33,74 +31,74 @@ def add_arguments(parser):
     
     parser.add_argument('Rock0',
                         type=rock_properties_type, 
-                        help='Rock properties of upper rock '+
-                        '[Vp,Vs, rho]',
+                        help='Upper rock type',
                         required=True,
                         default='2000,1000,2200'
                         )
                         
     parser.add_argument('Rock1',
                         type=rock_properties_type, 
-                        help='Rock properties of middle rock ' +
-                        '[Vp, Vs, rho]',
+                        help='Channel rock type',
                         required=True,
                         default='2200,1100,2300'
                         )
     
     parser.add_argument('Rock2',
                         type=rock_properties_type, 
-                        help='Rock properties of lower rock ' +
-                        '[Vp, Vs, rho]',
+                        help='Lower rock type',
                         required=False,
                         default='2500,1200,2600'
                         )
-    
-
-
+    parser.add_argument('trace',
+                        type=int, 
+                        help='Trace location',
+                        required=False,
+                        default=40
+                        )
+                        
 
 
                         
     return parser
 
-
 def run_script(args):
     from modelr.constants import dt, duration
-    
     matplotlib.interactive(False)
+    
+    """if args.transparent == 'False' or args.transparent == 'No':
+        transparent = False
+    else:
+        transparent = True"""
 
     args.ntraces = 300
     args.pad = 150
     args.reflectivity_method = zoeppritz
-    args.title = 'Scenario A - Cross Section'
-    args.theta = 0
-    args.f = 25.0
+    args.title = 'Wedge Model - Wavelet Cross Section'
+    args.theta = 0.0
     args.colourmap = 'Greys'
     args.wavelet = ricker
     args.wiggle_skips = 10
     args.aspect_ratio = 1
-    args.margin=1
     args.left = (0,0)
     args.right = (0,50)
-    args.slice='spatial'
-    args.trace = 0
+    args.margin=1
+    args.f = (15,75,.5)
+    args.slice='frequency'
+   
     
-    left = (args.left[0], args.left[1])
-    right = (args.right[0], args.right[1])
-
-
+    transparent = False
+    # This is a hack to conserve colors
     l1 = (150,110,110)
     l2 = (110,150,110)
     l3 = (110,110,150)
     layers= [l1,l2]
-
-    # This is a hack to conserve colors
-    colourmap = { rgb(l1[0],l1[1],l1[2]): args.Rock0,
-                  rgb(l2[0],l2[1],l2[2]): args.Rock1 }
+    colourmap = { rgb(150,110,110): args.Rock0,
+                  rgb(110,150,110): args.Rock1 }
     
     if not isinstance(args.Rock2, str):
-        colourmap[rgb( l3[0],l3[1],l3[2])] = args.Rock2
+        colourmap[rgb( 110,110,150)] = args.Rock2
         layers.append( l3 )
-    
+    # Get the physical model (an array of rocks)    
     model = mb.body( traces = args.ntraces,
                      pad = args.pad,
                      margin=args.margin,
@@ -109,9 +107,10 @@ def run_script(args):
                      layers = layers
                    )
 
-    return modelr_plot( model, colourmap, args )
-
     
+    return modelr_plot( model, colourmap, args )
+  
+
 def main():
     parser = ArgumentParser(usage=short_description,
                             description=__doc__
@@ -128,4 +127,3 @@ def main():
     
 if __name__ == '__main__':
     main()
-
