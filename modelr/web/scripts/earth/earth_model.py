cccb33ac3a57977ac2c4bf08f87aa5b4e8fb739a
EvanBianco <evan@agilegeoscience.com>
2014-06-26 12:59:47 -0300

    switched order of parser args

--- modelr/web/scripts/earth/earth_model.py
+++ modelr/web/scripts/earth/earth_model.py
@@ -7,13 +7,22 @@ short_description = ('Define the physical limits, domain, and reflectivity model
 def add_arguments(parser):
 
     
-    parser.add_argument('depth', type=float, default=1000.0,
-                       help="Z range of the model")
-    
     parser.add_argument('units', type=str, default='depth',
-                        help="Model domain",
+                        help="z-axis domain",
                         choices=['time', 'depth'])
 
+    parser.add_argument('depth', type=float, default=1000.0,
+                       help="z-range of model")
+
+    parser.add_argument('hor_domains', type=str, default='spatial only',
+                        help="horizontal domain(s)",
+                        choices=['spatial only', 
+                                 'spatial, angle, and frequency', 
+                                 #'spatial, and frequency',# not implemented yet
+                                 #'spatial, and angle'   # not implemented yet
+                                 ]
+                                 )    
+    
     parser.add_argument('reflectivity_method',
                         type=reflectivity_type,
                         help='Reflectivity model',
