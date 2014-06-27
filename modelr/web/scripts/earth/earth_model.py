7dcc7c348472011993161c732687fa82c30baea7
ben-bougher <ben.bougher@gmail.com>
2014-06-27 10:33:13 -0300

    removed domain parameters

--- modelr/web/scripts/earth/earth_model.py
+++ modelr/web/scripts/earth/earth_model.py
@@ -13,15 +13,6 @@ def add_arguments(parser):
 
     parser.add_argument('depth', type=float, default=1000.0,
                        help="z-range of model")
-
-    parser.add_argument('hor_domains', type=str, default='spatial only',
-                        help="horizontal domain(s)",
-                        choices=['spatial only', 
-                                 'spatial, angle, and frequency', 
-                                 #'spatial, and frequency',# not implemented yet
-                                 #'spatial, and angle'   # not implemented yet
-                                 ]
-                                 )    
     
     parser.add_argument('reflectivity_method',
                         type=reflectivity_type,
