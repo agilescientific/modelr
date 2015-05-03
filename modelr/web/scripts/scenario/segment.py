f80ce21ca6cb37081f59144064988d1ce55e40ab
Evan Saltman <evan.saltman@gmail.com>
2015-05-03 15:18:00 -0600

    updated segment file

--- modelr/web/scripts/scenario/segment.py
+++ modelr/web/scripts/scenario/segment.py
@@ -1,41 +1,65 @@
 import numpy as np
+import scipy as sp
 import matplotlib.pyplot as plt
-from skimage import io, color, segmentation
-from skimage.morphology import disk, dilation, closing
+from skimage import io, color, segmentation, exposure
+from skimage.morphology import disk, opening, dilation, erosion, skeletonize
 from skimage.measure import label
 
-from modelr.web.util import get_figure_data
 
 # import screen capture of a sketched geo model
 def image_segment(img_location):
     im=io.imread(img_location)
 
+    im = color.rgb2gray(im[0:-1:2, 0:-1:2])
+
+    # compressor to enhance hard edges
+    # square each item to obtain the power function
+    sqr = im**2
+    # Mild gaussian smooth of square -  not so much as to loose sensitivity to amplitude variations
+    flt2 = sp.ndimage.filters.gaussian_filter(sqr,21)
+    # Apply compressor -  divide the intensity of each original pixel by the square root of smoothed square
+    cmprs= im/(np.sqrt(flt2))
+
+    # contrast stretching
+    p2, p98 = np.percentile(cmprs, (2, 98))
+    rescale = exposure.rescale_intensity(cmprs, in_range=(p2, p98))
+
     # binarize image with scalar threshold
-    cap = color.rgb2gray(im) > 0.5
-    minsize=np.amin(np.shape(cap))
+    cap = ~(color.rgb2gray(rescale) > 0.5)
 
-    # clean up and remove white speckles within black edges with dilation
-    elsize=minsize/200
-    selem= disk(elsize)
-    dilated = dilation(~cap, selem)
+    # erosion - eliminates small white speckles
+    size=3
+    selem= disk(size)
+    eroded = erosion(cap, selem)
 
-    # Clear the objects connected to the image border (in this case just the outside white rim)
-    clean_border = segmentation.clear_border(~dilated)
+    # dilation - connects edge segments
+    # erosion - thins dilated edges
+    size1=10
+    selem= disk(size1)
+    dilated = dilation(eroded, selem)
+    eroded1 = erosion(dilated, selem)
 
-    # label objects in the cleaned image
+    # skeletonize and dilate to get final boundaries
+    edges1 = skeleton_th=skeletonize(eroded1)
+    s = disk(3)
+    openedsk = ~dilation(edges1,s)
+
+    # clear the objects connected to the image border (in this case just the outside white rim)
+    clean_border = segmentation.clear_border(~openedsk)
+
+    # label objects in the cleared image
     all_labels, n = label(clean_border, return_num=True)
 
     # autocrop by eliminating all empty rows and columns on the outside of the image
     non_empty_columns = np.where(all_labels.max(axis=0)>0)[0]
     non_empty_rows = np.where(all_labels.max(axis=1)>0)[0]
-    cropBox = (min(non_empty_rows)+60, max(non_empty_rows)-60, min(non_empty_columns)+60, 
-               max(non_empty_columns)-60)
+    cropBox = (min(non_empty_rows)+120, max(non_empty_rows)-120, min(non_empty_columns)+120, 
+               max(non_empty_columns)-120)
     crop = all_labels[cropBox[0]:cropBox[1]+1, cropBox[2]:cropBox[3]+1 ]
 
     # use dilation to enlarges bodies and shrinks edges between bodies
-    selem2=disk(elsize*3)
+    selem2=disk(15)
     dilated = dilation(crop, selem2)
-
 
     def closest(x,y, pixels, offset, best_colours):
         """
@@ -46,8 +70,6 @@ def image_segment(img_location):
         :param y: The y coordinate of the pixel
         :param offset: The offset to use to the search space.
         :param best_colours: List/Tuple of allowable colours
-        
-        :author ben bougher
 
         :returns: the nearest colour from the best colours set
         """
@@ -66,9 +88,9 @@ def image_segment(img_location):
                                   np.arange(x_low, x_high)))
 
         y_index =       np.concatenate((np.arange(y_low,y_high,dtype='int'),
-                                        np.ones(x_high-x_low,dtype='int')*y_high,
-                                        np.arange(y_low,y_high,dtype='int'),
-                                        np.ones(x_high-x_low, dtype='int')*y_low))
+                          np.ones(x_high-x_low,dtype='int')*y_high,
+                          np.arange(y_low,y_high,dtype='int'),
+                          np.ones(x_high-x_low, dtype='int')*y_low))
 
 
         data = pixels[x_index.astype('int'),
@@ -83,26 +105,12 @@ def image_segment(img_location):
 
         return best_colours[np.argmax(counts)]
 
-
     # find locations of borders between bodies - e.g. value is zero ~= color  black
     # replace those values with the value of the closest body
-
-    newest = dilated
+    newest=dilated
     index = np.array(np.where(dilated == 0))
 
     for x,y in zip(index[0], index[1]):
         newest[x,y] = closest(x,y, dilated, 50,[1,2,3])
 
-
-    # plot and save as .png
-
-    fig = plt.figure(figsize=(7, 7))
-    ax = fig.add_subplot(1, 1, 1)
-
-    plt.imshow(newest, cmap='Dark2')
-
-    ax.set_xticks([])
-    ax.set_yticks([])
-
-    ##plt.savefig('model.png',dpi=300, bbox_inches='tight', pad_inches=0)
     return newest
