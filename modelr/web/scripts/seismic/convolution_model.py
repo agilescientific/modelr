faf9f60d5d215032519453debc1f91af0531d391
Ben Bougher <ben.bougher@gmail.com>
2015-09-04 14:41:38 -0300

    added hacks for wavelet and angle gathers

--- modelr/web/scripts/seismic/convolution_model.py
+++ modelr/web/scripts/seismic/convolution_model.py
@@ -10,10 +10,36 @@ def run_script(json_payload):
     earth_model = ImageModelPersist.from_json(json_payload["earth_model"])
     seismic = Seismic.from_json(json_payload["seismic"])
 
-    data = do_convolve(seismic.src, earth_model.rpp_t(seismic.dt)).squeeze()
+    # todo get from json
+    trace = 50
+    offset = 2
+    data = do_convolve(seismic.src,
+                       earth_model.rpp_t(seismic.dt)[..., offset]
+                       [..., np.newaxis]).squeeze()
 
+    f0 = 4.0
+    f1 = 100.0
+    f = np.logspace(max(np.log2(f0), np.log2(7)),
+                    np.log2(f1), 50,
+                    endpoint=True, base=2.0)
+    wavelets = seismic.wavelet(.1, seismic.dt, f)
+
+    wavelet_gather = do_convolve(wavelets,
+                                 earth_model.rpp_t(seismic.dt)[...,
+                                                               trace, offset]
+                                 [..., np.newaxis,
+                                  np.newaxis]).reshape(data.shape[0],
+                                                       f.size)
+
+    offset_gather = do_convolve(
+        seismic.src, earth_model.rpp_t(seismic.dt)[..., trace, :]
+        [..., np.newaxis, ...]).reshape(data.shape[0], len(earth_model.theta))
+    
     payload = {"seismic": data[..., 0].T.tolist(), "dt": seismic.dt,
                "min": float(np.amin(data)), "max": float(np.amax(data)),
-               "dx": earth_model.dx}
+               "dx": earth_model.dx,
+               "wavelet_gather": wavelet_gather.T.tolist(),
+               "offset_gather": offset_gather.T.tolist()}
 
+    print offset_gather.shape, wavelet_gather.shape
     return payload
